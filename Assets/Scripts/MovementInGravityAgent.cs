38d4f3dbc584ae21dfbea0cbd0f284aab5099090
boulygin233 <boulygin.artem@gmail.com>
2021-02-16 08:13:29 +0300

    Fixed HW and Grid work

--- Assets/Scripts/MovementInGravityAgent.cs
+++ Assets/Scripts/MovementInGravityAgent.cs
@@ -15,6 +15,7 @@ public class MovementInGravityAgent : MonoBehaviour
     [SerializeField] private float gravity_Diam;
     private GameObject gravity;
     private const float TOLERANCE = 0.1f;
+    private const float ACCELERATION_LIMIT = 5;
 
     private void Start()
     {
@@ -22,27 +23,39 @@ public class MovementInGravityAgent : MonoBehaviour
         transform.position = m_Start;
         gravity.transform.position = gravity_Position;
         gravity.transform.localScale = new Vector3(gravity_Diam, gravity_Diam, gravity_Diam);
+        transform.localScale = new Vector3(m_Diam, m_Diam, m_Diam);
     }
 
     // Update is called once per frame
     void Update()
     {   
-        
-        gravity.transform.position = gravity_Position;
+        /*gravity.transform.position = gravity_Position;
         gravity.transform.localScale = new Vector3(gravity_Diam, gravity_Diam, gravity_Diam);
-        transform.localScale = new Vector3(m_Diam, m_Diam, m_Diam);
-        float distance = (gravity_Position - transform.position).magnitude;
+        transform.localScale = new Vector3(m_Diam, m_Diam, m_Diam);*/
+        
         Vector3 acceleration = gravity_G * gravity_Weight * (gravity_Position - transform.position) /
                                Mathf.Pow((gravity_Position - transform.position).magnitude, 3);
-        Vector3 target = transform.position + m_Speed * Time.deltaTime + acceleration * Mathf.Pow(Time.deltaTime, 2) / 2;
-        Vector3 dir = (target - transform.position).normalized;
-        if (distance - gravity_Diam / 2 < TOLERANCE)
+
+        if (acceleration.magnitude > ACCELERATION_LIMIT)
+        {
+            acceleration = acceleration.normalized * ACCELERATION_LIMIT;
+        }
+        
+        Vector3 target = transform.position + m_Speed * Time.deltaTime + acceleration *
+            (Mathf.Pow(Time.deltaTime, 2) * 0.5f);
+        
+        /*Vector3 dir = (target - transform.position).normalized;
+        Vector3 delta = dir * (m_Speed.magnitude * Time.deltaTime);
+        */
+        /*float distance = (gravity_Position - transform.position).magnitude;
+         if (distance - gravity_Diam * 0.5f - m_Diam * 0.5f < TOLERANCE)
         {
             m_Speed = dir * 100;
-        }
-        Vector3 delta = dir * (m_Speed.magnitude * Time.deltaTime);
+        }*/
+        
         m_Speed += acceleration * Time.deltaTime;
-        transform.Translate(delta);
+        //transform.Translate(delta);
+        transform.position = target;
     }
     
 }
